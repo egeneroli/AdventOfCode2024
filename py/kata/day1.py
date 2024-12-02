d4c90880162c73e855619d4c0db0a65bf699f490
Evan Generoli <evan.generoli@siliconmtn.com>
2024-12-02 16:18:30 -0700

    finished day1

--- py/kata/day1.py
+++ py/kata/day1.py
@@ -2,27 +2,38 @@ from py.util import read_input
 
 # read in data
 lines: list[str] = read_input("input1")
-# print(lines[0:10])
+print(lines[0:10])
 
 # parse data
 def parse_data(x: list[str]) -> list[list[int]]:
     return [[int(x) for x in line.split()] for line in x]
 
 data: list[list[int]] = parse_data(lines)
+list1: list[int] = [row[0] for row in data]
+list2: list[int] = [row[1] for row in data]
 # print(data[0:10])
 
 # calculate differences
-def calc_diffs(x: list[list[int]]) -> list[int]:
-    list1: list[int] = [x[0] for x in x]
+def calc_diffs(list1: list[int], list2: list[int]) -> list[int]:
     list1.sort()
-    list2: list[int] = [x[1] for x in x]
     list2.sort()
     return [abs(x1 - x2) for x1, x2 in zip(list1, list2)]
 
-diffs: list[int] = calc_diffs(data)
+diffs: list[int] = calc_diffs(list1, list2)
 # print(diffs[0:10])
 
-print(f"result: {sum(diffs)}")
+# part 1 solution
+print(f"result 1: {sum(diffs)}\n")
+
+## part 2
+def calc_sim_scores(left: list[int], right: list[int]) -> list[int]:
+    return [x * right.count(x) for x in left]
+
+sims: list[int] = calc_sim_scores(list1, list2)
+print(sims[0:10])
+
+# part 2 solution
+print(f"result 2: {sum(sims)}")
 
 
 
