b9cd88bf30d23f58dea79e7e2a0988824105a384
Evan Generoli <evan.generoli@siliconmtn.com>
2024-12-03 20:15:41 -0700

    worked on day 3 part 2, not done yet

--- py/kata/day3.py
+++ py/kata/day3.py
@@ -1,5 +1,6 @@
 import math
-import re
+import regex as re
+# import re
 
 from py.util import read_input, parse_matrix
 
@@ -13,7 +14,7 @@ def part1() -> int:
     # print(f"matches: {matches}")
 
     # parse each mul instruction
-    instructions: list[list[int]] = [[int(num) for num in match.split(",")] for match in matches]
+    instructions: list[list[int]] = [[int(num) for num in re.findall("\d+", match)] for match in matches]
     # print(f"instructions: {instructions}")
 
     # calculate products
@@ -29,18 +30,31 @@ print(f"result 1: {part1()}\n")
 # part 2 solution
 def part2() -> int:
     # use regex to match all mul instructions
-    matches: list[str] = re.findall("(?<=mul\()\d+,\d+(?=\))", input_str)
-    # print(f"matches: {matches}")
+    data: str = f"do(){input_str}don't()"
+    # print(f"input: {data}")
+    # matches: list[str] = re.findall("(?<=do\(\))[^d]*(?:[^d]|d(?!on't))*mul\(\d+,\d+\)", data)
+    matches: list[str] = re.findall("(do\(\)).*?(mul\(\d+,\d+\).*?)+", data)
+    # pattern = re.compile("(?:do\(\))(.*?(?:mul\(\d+,\d+\)))+")
+    # matches = []
+    # lst: list = [match for match in pattern.finditer(data)]
+    # for match in lst[:1]:
+    #     # matches.extend(re.findall("mul\(\d+,\d+\)", match.group(0)))
+    #     print(match.allcaptures()[:1])
 
-    # parse each mul instruction
-    instructions: list[list[int]] = [[int(num) for num in match.split(",")] for match in matches]
-    # print(f"instructions: {instructions}")
+    # matches: list[str] = [x for match in re.findall("(?<=do\(\)).*?(?=do)", data) for x in re.findall("mul\(\d+,\d+\)", match)]
+    print(f"matches: {matches.__len__()}")
+    for match in matches[:3]:
+        print(f"match: {match}")
 
-    # calculate products
-    results: list[int] = [math.prod(instruction) for instruction in instructions]
-
-    # calculate final sum
-    return sum(results)
+    # # parse each mul instruction
+    # instructions: list[list[int]] = [[int(num) for num in re.findall("\d+", match)] for match in matches]
+    # # print(f"instructions: {instructions}")
+    #
+    # # calculate products
+    # results: list[int] = [math.prod(instruction) for instruction in instructions]
+    #
+    # # calculate final sum
+    # return sum(results)
 
 print(f"result 2: {part2()}")
 
