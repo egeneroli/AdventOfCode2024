d165e226afb9a56044bef33b3b97e1d5f7c88a1f
Evan Generoli <evan.generoli@siliconmtn.com>
2024-12-08 17:27:59 -0700

    commit latest

--- py/kata/day6B.py
+++ py/kata/day6B.py
@@ -24,7 +24,7 @@ def step(pos: tuple[int, int, int]) -> tuple[int, int, int]:
     return x, y, dir
 
 import re
-def get_pos() -> tuple[int, int, int]:
+def get_pos(mat: list[str] = mat) -> tuple[int, int, int]:
     # find position char and return position
     for x in range(len(mat)):
         row = mat[x]
@@ -46,13 +46,13 @@ class OffGrid(Exception):
     # class for OffGridException
     pass
 
-def mark(x: int, y: int, marker: str) -> None:
+def mark(x: int, y: int, marker: str, mat: list[str] = mat) -> None:
     mat[x] = mat[x][:y] + marker + mat[x][y+1:]
 
-def get_char(x: int, y: int) -> str:
+def get_char(x: int, y: int, mat: list[str] = mat) -> str:
     return mat[x][y]
 
-def move(pos: tuple[int, int, int]) -> tuple[int, int, int]:
+def move(pos: tuple[int, int, int], mat: list[str] = mat) -> tuple[int, int, int]:
     # try to move forward in direction already facing
     # get candidate position
     new_pos: tuple[int, int, int] = step(pos)
@@ -79,27 +79,77 @@ def move(pos: tuple[int, int, int]) -> tuple[int, int, int]:
 
     return new_pos
 
-def part1():
+class InfiniteLoop(Exception):
+    pass
+
+def patrol(history: list[tuple[int, int, int]] = [], mat: list[str] = mat):
     # iteratively step while still in bounds
     # while on grid, move -- if OB, end
-    pos: tuple[int, int, int] = get_pos()
+    pos: tuple[int, int, int] = get_pos(mat)
+    # history: list[tuple[int, int, int]] = []
+    history.append(pos)
     # print(f"pos: {pos}")
     while True:
         try:
-            pos = move(pos)
+            pos = move(pos, mat)
             # print(f"pos: {pos}")
+            if pos in history:
+                raise InfiniteLoop
+            history.append(pos)
         except OffGrid:
             break
-    return sum(line.count("X") for line in mat) + 1
+    return history
 
-print(f"result 1: {part1()}\n")
+def get_unique_locs(history: list[tuple[int, int, int]]) -> set[tuple[int, int]]:
+    return set((x, y) for x, y, _ in patrol(history))
+
+def part1(history: list[tuple[int, int, int]]) -> int:
+    return len(get_unique_locs(history))
+
+history: list[tuple[int, int, int]] = []
+# print(f"result 1: {part1(history)}\n")
 for row in mat:
     print(row)
 
 ## part 2
-# part 2 solution
-def part2() -> int:
-    return -1
+def part2():
+    # iteratively step while still in bounds
+    # while on grid, move -- if OB, end
+    pos: tuple[int, int, int] = get_pos()
+    # print(f"pos: {pos}")
+    count: int = 0
+    locations: set[tuple[int, int]] = get_unique_locs(history)
+    print(f"locations len: {len(locations)}")
+    # for pos in locations:
+    for pos in [(6,3),(7,6),(7,7),(8,1),(8,4),(9,7)]:
+        # put an obstacle in location and see if it results in loop
+        X: list[str] = input_str.split("\n").copy()
+        mark(*pos, '#', X)
+        print(f"\npos: {pos}")
+        # for row in X:
+        #     print(row)
+        # print()
+        try:
+            patrol(mat=X)
+            for row in X:
+                print(row)
+            print()
+        except InfiniteLoop:
+            print("Loop")
+            count += 1
+    return count
+
+    # while True:
+    #     try:
+    #         # before each step, check if putting obstacle in front of guard results in loop
+    #
+    #         # while on grid, move
+    #         pos = move(pos)
+    #
+    #         # print(f"pos: {pos}")
+    #     except OffGrid:
+    #         break
+    # return sum(line.count("X") for line in mat) + 1
 
 print(f"result 2: {part2()}")
 
